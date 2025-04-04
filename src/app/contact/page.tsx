8d272cf8ffb23b4606ea12baeb93fdbe4dc0f1b5
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-04-04 17:17:08 +0530

    last commang

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
+"use client";
+import React from "react";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 function page() {
   return (
-    <div>text 2</div>
-  )
+    <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold capitalize mb-8">
+          contact us
+        </h1>
+        <p></p>
+        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+          Welcome to MailJet, the best transactional email service on the web.
+          We provide reliable, scalable, and customizable email solutions for
+          your business.
+        </p>
+        <input
+          type="text"
+          placeholder="hi@manuarora.in"
+          className="rounded-lg border py-4 px-6 border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+        />
+        <textarea
+          placeholder="Your message here..."
+          className="rounded-lg border py-4 px-6 border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+        />
+        <div className="w-full">
+          <button className="bg-gradient-to-b from-teal-500 to-teal-600 text-white font-semibold py-2 px-4 rounded-lg mt-4 w-full relative z-10 hover:shadow-lg transition duration-200 ease-in-out">
+            Send
+          </button>
+        </div>
+      </div>
+      <BackgroundBeams />
+    </div>
+  );
 }
 
-export default page
+export default page;
